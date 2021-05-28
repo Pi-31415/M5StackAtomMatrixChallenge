9814519fb9df42a150cdb43fd6a7645b6df319cd
Pi <paingthetko.pi@gmail.com>
2021-05-29 00:11:18 +0630

    Use arrays to manage displays

--- M5StackAtomMatrixChallenge/M5StackAtomMatrixChallenge.ino
+++ M5StackAtomMatrixChallenge/M5StackAtomMatrixChallenge.ino
@@ -1,13 +1,32 @@
 /**
- * M5StackAtomMatrixChallenge.ino
- *
- * By: Umer Bin Liaqat, Omar Elkammah, Sherifa Yakubu, Pi Ko
- * Date: 28 May 2021
- *
- * Assignment 2 of ENGR-UH 1021J: Design & Innovation Summer 2021
- */
+   M5StackAtomMatrixChallenge.ino
+
+   By: Umer Bin Liaqat, Omar Elkammah, Sherifa Yakubu, Pi Ko
+   Date: 28 May 2021
+
+   Assignment 2 of ENGR-UH 1021J: Design & Innovation Summer 2021
+*/
 
 #include "M5Atom.h"
+
+//Define Colors
+int GRB_COLOR_WHITE = 0xffffff;
+int GRB_COLOR_BLACK = 0x000000;
+int GRB_COLOR_RED = 0x00ff00;
+
+//Define Patterns
+int full_screen[25] =
+{
+  1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1
+};
+
+//Array of patterns for display
+int *display[6] = { full_screen };
+
 
 void setup()
 {
@@ -31,70 +50,23 @@ void loop()
 
   if (STEP == 0)
   {
-    //Nothing on screen
+    //Nothing on screen, blank (Step 1)
     BLINK_MODE_ON = false;
     M5.dis.clear();
-    M5.dis.drawpix(0, 0x00f000);
   }
   else if (STEP == 1)
   {
-    //Only 1 red LED
-    BLINK_MODE_ON = false;
+    //Flash Red (Step 2)
+    BLINK_MODE_ON = true;
     M5.dis.clear();
-    M5.dis.drawpix(2, 0x00f000);
+    drawArray2(display[0], GRB_COLOR_RED);
   }
   else if (STEP == 2)
   {
-    //Should paint the whole screen red
+    //Flash Red (Step 2)
     BLINK_MODE_ON = true;
     M5.dis.clear();
-    M5.dis.drawpix(0, 0x00f000);
-    M5.dis.drawpix(1, 0x00f000);
-    M5.dis.drawpix(2, 0x00f000);
-    M5.dis.drawpix(3, 0x00f000);
-    M5.dis.drawpix(4, 0x00f000);
-    M5.dis.drawpix(5, 0x00f000);
-    M5.dis.drawpix(6, 0x00f000);
-    M5.dis.drawpix(7, 0x00f000);
-    M5.dis.drawpix(8, 0x00f000);
-    M5.dis.drawpix(9, 0x00f000);
-    M5.dis.drawpix(10, 0x00f000);
-    M5.dis.drawpix(11, 0x00f000);
-    M5.dis.drawpix(12, 0x00f000);
-    M5.dis.drawpix(13, 0x00f000);
-    M5.dis.drawpix(14, 0x00f000);
-    M5.dis.drawpix(15, 0x00f000);
-    M5.dis.drawpix(16, 0x00f000);
-    M5.dis.drawpix(17, 0x00f000);
-    M5.dis.drawpix(18, 0x00f000);
-    M5.dis.drawpix(19, 0x00f000);
-    M5.dis.drawpix(20, 0x00f000);
-    M5.dis.drawpix(21, 0x00f000);
-    M5.dis.drawpix(22, 0x00f000);
-    M5.dis.drawpix(23, 0x00f000);
-    M5.dis.drawpix(24, 0x00f000);
-    M5.dis.drawpix(25, 0x00f000);
-  }
-  else if (STEP == 3)
-  {
-    //Only 1 red LED
-    BLINK_MODE_ON = false;
-    M5.dis.clear();
-    M5.dis.drawpix(3, 0x00f000);
-  }
-  else if (STEP == 4)
-  {
-    //Only 1 red LED
-    BLINK_MODE_ON = false;
-    M5.dis.clear();
-    M5.dis.drawpix(4, 0x00f000);
-  }
-  else if (STEP == 5)
-  {
-    //Only 1 red LED
-    BLINK_MODE_ON = false;
-    M5.dis.clear();
-    M5.dis.drawpix(5, 0x00f000);
+    drawArray2(display[0], GRB_COLOR_WHITE);
   }
 
   //This is for debugging purpose
@@ -109,4 +81,13 @@ void loop()
 
   delay(50);
   M5.update();
+}
+
+
+void drawArray2(int arr[], int color)
+{
+  for (int i = 0; i < 25; i++)
+  {
+    M5.dis.drawpix(i, color);
+  }
 }

80100fa08cb2ef795a450809304ab7fa90468a50
Pi <paingthetko.pi@gmail.com>
2021-05-29 21:27:14 +0630

    Temperature Sensing

--- tempvsvoltage/tempvsvoltage.ino
+++ tempvsvoltage/tempvsvoltage.ino
@@ -1,22 +1,64 @@
-#include <M5Stack.h>
-int RawValue = 0;
-double volatge = 0;
-double tempC = 0;
-double Sum;
-double minTemp = 200;
-double maxTemp = 0;
+#include "M5Atom.h"
+
+int GRB_COLOR_WHITE = 0xffffff;
+int GRB_COLOR_BLACK = 0x000000;
+int GRB_COLOR_RED = 0x00ff00;
+int GRB_COLOR_ORANGE = 0xa5ff00;
+int GRB_COLOR_YELLOW = 0xffff00;
+int GRB_COLOR_GREEN = 0xff0000;
+int GRB_COLOR_BLUE = 0x0000ff;
+int GRB_COLOR_PURPLE = 0x008080;
+
+float lowTempF = 103;
+float highTempF = 105;
+
+float tempC = 0;
+bool IMU6886Flag = false;
 
 void setup()
 {
-    serial.begin(9600);
-    M5.begin();
-    M5.Power.begin();
-    M5.Lcd.fillScreen(WHITE);
-    M5.Lcd.setTextColor(BLACK);
-    M5.Lcd.setTextSize(2);
-    M5.Lcd.setCursor(40, 10);
-    M5.Lcd.printf("M5stack Thermometer");
-    M5.Lcd.fillCircle(65, 180, 30, RED);
-    M5.Lcd.fillRect(55, 60, 20, 150, RED);
-    M5.Lcd.fillCircle(65, 60, 10, RED);
+    M5.begin(true, false, true);
+    delay(20);
+
+    IMU6886Flag = M5.IMU.Init() == 0;
+
+    if (!IMU6886Flag)
+    {
+        Serial.println("Error initializing the IMU! :-(");
+    }
+}
+
+void loop()
+{
+    if (IMU6886Flag)
+    {
+        M5.IMU.getTempData(&tempC);
+        Serial.printf(" Temp : %.2f C \r\n", tempC);
+        float tempF = tempC * 9 / 5 + 32;
+        Serial.printf(" Temp : %.2f F \r\n", tempF);
+
+        if (tempF < lowTempF)
+        {
+            fillDisplay(GRB_COLOR_BLUE);
+        }
+        else if (tempF > highTempF)
+        {
+            fillDisplay(GRB_COLOR_RED);
+        }
+        else
+        {
+            fillDisplay(GRB_COLOR_GREEN);
+        }
+
+        delay(500);
+        M5.update();
+    }
+}
+
+void fillDisplay(int fillColor)
+{
+    for (int i = 0; i < 25; i++)
+    {
+        M5.dis.drawpix(i, fillColor);
+    }
 }
